366cc73848467df45b18c5e7423701b40505cfaa
yichenljing <40904846+yichenljing@users.noreply.github.com>
2019-12-13 05:50:57 -0500

    Change scene

---
+++ codeLab0 Final/Assets/scripts/changeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    // Use this for initialization
+
+
+    public string sceneName;
+    public void Click()
+    {
+        Debug.Log("enter" + sceneName + "level");
+        Application.LoadLevel(sceneName);
+    }
+}
+
